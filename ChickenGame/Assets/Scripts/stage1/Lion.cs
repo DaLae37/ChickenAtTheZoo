7dc2c8f563f146bf88f4104b5ad6a9689cdf5372
LeeRoUn <dlfhdns13@naver.com>
2017-07-14 19:23:02 +0900

    addLion

--- ChickenGame/Assets/Scripts/stage1/Lion.cs
+++ ChickenGame/Assets/Scripts/stage1/Lion.cs
@@ -5,43 +5,18 @@ using UnityEngine;
 public class Lion : MonoBehaviour {
 
 	public float speed = 0.3f; 
-
-	public bool view = true;
-
-	private int touched = 0;
-
+	public float range = 10f;
 
 
 	void FixedUpdate () {
-		//if (Player.instance.onLionGround == true) {
-		//	if (((Player.instance.view == true) && (view == false))||((Player.instance.view == false) && (view == true))) {
-		//	} else {
-		//		LionMove();
-		//	}
-		//} else {
-		//	LionMove();
-		//}
+		float dst = Vector3.Distance (transform.position, PlayerControl.instance.playerList[PlayerControl.instance.ctrPlayerIndex].transform.position);
 
+		if (dst < range) {
+			if(transform.position.x > PlayerControl.instance.playerList[PlayerControl.instance.ctrPlayerIndex].transform.position.x)
+				transform.Translate (new Vector3 (-speed*Time.deltaTime, 0, 0));
+			else
+				transform.Translate (new Vector3 (speed*Time.deltaTime, 0, 0));
+		}
 	}
 
-	void LionMove(){
-		if (view == true) {
-			transform.Translate (new Vector2 (speed * Time.smoothDeltaTime, 0));
-		}
-		if (view == false) {
-			transform.Translate (new Vector2 (-speed * Time.smoothDeltaTime, 0));
-		}
-	}
-	void OnCollisionEnter2D(Collision2D coll){
-		touched++;
-		if (touched >= 2) {
-			if (view == true) {
-				view = false;
-				touched-=2;
-			} else {
-				view = true;
-				touched--;
-			}
-		}
-	}
 }
